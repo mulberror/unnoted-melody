a633ed5266637299714c044c92f4242e13866bd3
mulberror <mulberror@gmail.com>
2024-11-25 18:03:58 +0800

    fix: update the font config

--- newpost.py
+++ newpost.py
@@ -4,9 +4,9 @@ import shutil
 
 # python3 blog createPost <name> [-e]
 # 创建一篇文章
-def newpost(en_name, year, season):
+def newpost(en_name, year, month):
     post_name = en_name
-    path = f"posts/{year}/{season}/{post_name}"
+    path = f"posts/{year}/{month}/{post_name}"
     os.system(f"hugo new {path}/{post_name}.md")
     os.rename(f"./content/{path}/{post_name}.md", f"./content/{path}/index.zh-cn.md")
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("en_name")
     parser.add_argument("year")
-    parser.add_argument("season")
+    parser.add_argument("month")
 
     args = parser.parse_args()
-    newpost(args.en_name, args.year, args.season)
+    newpost(args.en_name, args.year, args.month)
